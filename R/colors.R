77a040439f5f6b9081e58c547cf23d5ce85ebde7
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2022-04-24 15:17:47 -0400

    Added some colors to be used with the package.

---
+++ R/colors.R
@@ -0,0 +1,15 @@
+#' Colors used within the `jtheme`
+#'
+#' * light_red :
+#' * light_blue :
+#' * red :
+#' * blue :
+#'
+#'
+#'@export
+colors = list(
+    light_red  = "#fddbc7",
+    light_blue = "#d1e5f0",
+    blue       = "#4393c3",
+    red        = "#d6604d"
+)
