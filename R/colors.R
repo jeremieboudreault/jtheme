7fd0e111db32f36b897b5137e075a6bb2b1e5e46
Jeremie Boudreault <jeremieboudreault11@gmail.com>
2022-05-15 10:12:34 -0400

    Added a light_grey color to the color list.

--- R/colors.R
+++ R/colors.R
@@ -4,13 +4,14 @@
 #' * light_blue :
 #' * red :
 #' * blue :
+#' * light_grey :
 #'
 #'
 #'@export
 colors = list(
     light_red  = "#fddbc7",
     light_blue = "#d1e5f0",
-    light_grey = "#F7F7F7",
     blue       = "#4393c3",
-    red        = "#d6604d"
+    red        = "#d6604d",
+    light_grey = "#F7F7F7"
 )
