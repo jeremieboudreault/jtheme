8a1841bb4756ceb4449f8d0919fac759fafd6e03
Jeremie Boudreault <jeremieboudreault11@gmail.com>
2022-09-27 16:07:56 -0400

    Initial commit on to_factor(). Allows to transform chr data to factor for plotting purpose. This function keeps the order of the values integrally.

---
+++ R/to_factor.R
@@ -0,0 +1,13 @@
+#' A small wrapper for factor function.
+#'
+#' `to_factor` is a wrapper for [base::factor()] that allows to easily convert
+#' a string character to some factors for plotting purpose.
+#'
+#'
+#' @param rev : Do you want to reverse the levels.
+#'
+#' @export
+to_factor <- function(x, rev = FALSE) {
+    levels <- if (rev) rev(unique(x)) else unique(x)
+    return(factor(x, levels))
+}
