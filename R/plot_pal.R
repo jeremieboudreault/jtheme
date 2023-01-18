b9ab77dc862a16b55366930b9f5ed68dd088669e
Jeremie Boudreault <jeremieboudreault11@gmail.com>
2023-01-18 10:39:24 -0500

    Added plot_pal() to jtheme, which allows to plot a palette of colors.

---
+++ R/plot_pal.R
@@ -0,0 +1,19 @@
+# plot_pal()
+
+#' A function to plot a palette of color.
+#'
+#' `plot_pal` takes a palette of colors of values and plot it.
+#'
+#' @param pal : A vector of colors.
+#'
+#' @export
+
+plot_pal <- function(pal) {
+    plot(
+        x  = seq_along(pal),
+        y  = rep(1L, length(pal)),
+        col = pal,
+        pch = 19,
+        cex = 5
+    )
+}
