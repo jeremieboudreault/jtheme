28906037ee569e62614a844839ee3c87e52d5711
Jeremie Boudreault <jeremieboudreault11@gmail.com>
2023-01-18 10:38:01 -0500

    Added list of the sizes inside a global file.

---
+++ R/globals.R
@@ -0,0 +1,14 @@
+# globals.R
+
+# Size of lists.
+sizes_map <- list(
+    rectsmall = c(5, 3.5),
+    rect      = c(7, 5),
+    rectbig   = c(8, 6),
+    rectxbig  = c(10, 8),
+    sqrsmall  = c(4, 3.5),
+    sqr       = c(6, 5),
+    sqrbig    = c(8, 7),
+    sqrxbig   = c(10, 9),
+    custom    = c(NA, NA)
+)
